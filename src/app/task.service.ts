397ed1ce6b745fe4c8ef29b1d227c4c12923229c
Yamini Gosula <yaminisurekha2002@gmail.com>
2024-12-29 19:14:31 +0530

    Before userId included

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -20,4 +20,35 @@ export class TaskService {
     const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
     return this.http.get('http://127.0.0.1:8000/api/tasks', { headers });
   }
+
+  getUsers():Observable<any>{
+    const token=this.getToken();
+    if(!token){
+      console.error('No token found');
+    }
+    const headers = new HttpHeaders({Authorization: `Bearer ${token}`});
+    return this.http.get('http://127.0.0.1:8000/api/show',{headers});
+  }
+
+  getAllTask():Observable<any>{
+    const token=this.getToken();
+    if(!token){
+      console.error('No token found');
+    }
+    const headers = new HttpHeaders({Authorization: `Bearer ${token}`});
+    return this.http.get('http://127.0.0.1:8000/api/taskTable',{headers});
+  }
+
+  addTask(payload:{user_id:string;task_id:any}):Observable<any>{
+    const token=this.getToken();
+    if(!token){
+      console.error('No token found');
+    }
+    const headers = new HttpHeaders({Authorization: `Bearer ${token}`});
+    return this.http.post('http://127.0.0.1:8000/api/assignTask',payload,{headers});
+  }
+
+ 
+
+
 }
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RoleService } from '../role.service';
 import { TaskResponse } from '../models/task.model';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task',
@@ -11,11 +12,23 @@ import { TaskResponse } from '../models/task.model';
 export class TaskComponent implements OnInit {
 
   tasks: TaskResponse | null = null;
+  Allusers: any[] = [];
+  Alltasks:any[]=[];
+  savedInfo: string = ''; 
 
-  constructor(private taskService: RoleService){}
+  selectedUser: string='';   
+  selectedTask: any = null;
+
+
+
+  constructor(private taskService: RoleService,private x:TaskService){}
 
   ngOnInit(): void{
     this.getTasks();
+    this.getUsers();
+    this.getTask();
+    // this.addTask();
+   
   }
 
   getTasks():void{
@@ -30,10 +43,73 @@ export class TaskComponent implements OnInit {
         console.error('Error fetchimg tasks:',error);
       }
 
-     
     );
-
   }
+
+  getUsers():void{
+    this.x.getUsers().subscribe(
+      (data:string[])=>{
+        console.log('Fetched users:',data);
+        this.Allusers=data;
+        console.log(this.Allusers);
+      }
+
+    )
+  }
+
+  getTask():void{
+    this.x.getAllTask().subscribe(
+      (data:any)=>{
+        console.log('Fetched tasks:',data);
+        this.Alltasks=data;
+        console.log(this.Alltasks);
+      }
+
+    )
+  }
+
+  addTask(): void {
+    const payload = {
+      user_id: this.selectedUser,
+      task_id: this.selectedTask,
+    };
+
+    this.x.addTask(payload).subscribe(
+      (response) => {
+        console.log('Task added successfully:', response);
+        
+        this.getTasks();
+        // this.savedInfo = `User: ${this.selectedUser}, Task: ${this.selectedTask}`;
+      },
+      (error) => {
+        console.error('Error adding task:', error);
+      }
+    );
+  }
+
+
+  saveSelection(): void {
+    console.log('Selected User:', this.selectedUser);
+    console.log('Selected Task:', this.selectedTask);
+    console.log(this.savedInfo);
+    this.savedInfo=`User: ${this.selectedUser}, Task: ${this.selectedTask}`;
+    console.log(this.savedInfo);
+  }
+
+
+  onTaskSelect(): void {
+    console.log('Selected Task:', this.selectedTask);  
+  }
+  
+  
+
+  
+
+  
+
+
+
+
 
  
  
